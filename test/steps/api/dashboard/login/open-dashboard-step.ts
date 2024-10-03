3e9782814da44b069c401e24efb80ebeb655d2ac
ariansyahriwendi <ariansyahriwendi@gmail.com>
2024-10-03 13:52:44 +0700

    name

--- test/steps/api/dashboard/login/open-dashboard-step.ts
+++ test/steps/api/dashboard/login/open-dashboard-step.ts
@@ -16,6 +16,23 @@ await browser.setCookies({
     value: `Bearer ${this.token}`  // Masukkan token login yang benar
 })
 
+await browser.setCookies({
+    name: `warehouse`,  // Sesuaikan dengan nama cookie yang digunakan oleh aplikasi
+    value: `{%22id%22:1}`
+})
+
+await browser.setCookies({
+    name: `user`,  // Sesuaikan dengan nama cookie yang digunakan oleh aplikasi
+    value: `{%22name%22:%22Aiansyah%20Riwendi%22%2C%22id%22:846%2C%22email%22:%22mariansyah.riwendi@nusantara.technology%22%2C%22status%22:1%2C%22warehouse_id%22:1%2C%22role_name%22:%22Product%20Tech%22%2C%22is_testing%22:0}`  // Masukkan token login yang benar
+})
+
+await browser.setCookies({
+    name: `g_state`,  // Sesuaikan dengan nama cookie yang digunakan oleh aplikasi
+    value: `{"i_l":0}`
+})
+
+await browser.refresh()
+
 await browser.pause(360000)
 
 })
